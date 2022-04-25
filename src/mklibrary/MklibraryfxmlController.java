6ffe3b4cb040da7e9be203188363b2875879d7da
titleCarterPillow/title <cahanson18@gmail.com>
2022-04-25 17:50:49 -0400

    Updated Search Window

--- src/mklibrary/MklibraryfxmlController.java
+++ src/mklibrary/MklibraryfxmlController.java
@@ -30,6 +30,32 @@ public class MklibraryfxmlController {
     }
 
     public void searchBooks() {
+        MKLibrary.getLibrary().add(new Book("The Hobbit", "J.R.R. Tolkien", 1937, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Lord of the Rings", "J.R.R. Tolkien", 1954, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Catcher in the Rye", "J.D. Salinger", 1951, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Grapes of Wrath", "John Steinbeck", 1939, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Scarlet Letter", "Nathaniel Hawthorne", 1850, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Lord of the Rings", "J.R.R. Tolkien", 1954, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Catcher in the Rye", "J.D. Salinger", 1951, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Grapes of Wrath", "John Steinbeck", 1939, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Scarlet Letter", "Nathaniel Hawthorne", 1850, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Hobbit", "J.R.R. Tolkien", 1937, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Lord of the Rings", "J.R.R. Tolkien", 1954, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Catcher in the Rye", "J.D. Salinger", 1951, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Grapes of Wrath", "John Steinbeck", 1939, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Scarlet Letter", "Nathaniel Hawthorne", 1850, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Hobbit", "J.R.R. Tolkien", 1937, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Lord of the Rings", "J.R.R. Tolkien", 1954, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Catcher in the Rye", "J.D. Salinger", 1951, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Grapes of Wrath", "John Steinbeck", 1939, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Scarlet Letter", "Nathaniel Hawthorne", 1850, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Hobbit", "J.R.R. Tolkien", 1937, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Lord of the Rings", "J.R.R. Tolkien", 1954, "A children's General Works novel", "General Works"));
+        MKLibrary.getLibrary().add(new Book("The Catcher in the Rye", "J.D. Salinger", 1951, "A children's General Works novel", "General Works"));
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("searchfxml.fxml"));
             Parent root = loader.load();
@@ -37,6 +63,8 @@ public class MklibraryfxmlController {
             MKLibrary.getSearchBookStage().setScene(new Scene(root));
 
             SearchBookFXMLController searchBookController = loader.getController();
+
+            searchBookController.createBookList();
 
             MKLibrary.getSearchBookStage().show();
         } catch (IOException ex) {
--- src/mklibrary/SearchBookFXMLController.java
+++ src/mklibrary/SearchBookFXMLController.java
@@ -1,5 +1,49 @@
 package mklibrary;
 
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 public class SearchBookFXMLController {
 
+    @FXML
+    private TableView bookList;
+
+    @FXML
+    private TableColumn<Book, String> titleColumn;
+
+    @FXML
+    private TableColumn<Book, String> authorColumn;
+
+    @FXML
+    private TableColumn<Book, String> genreColumn;
+
+    @FXML
+    private TableColumn<Book, String> yearColumn;
+
+    @FXML
+    private TableColumn<Book, String> deweyColumn;
+
+    private int selectedDewey = -1;
+
+    public void createBookList(){
+        System.out.print(MKLibrary.getLibrary());
+        titleColumn.setCellValueFactory(new PropertyValueFactory<Book, String>("title"));
+        authorColumn.setCellValueFactory(new PropertyValueFactory<Book, String>("author"));
+        genreColumn.setCellValueFactory(new PropertyValueFactory<Book, String>("genre"));
+        yearColumn.setCellValueFactory(new PropertyValueFactory<Book, String>("year"));
+        deweyColumn.setCellValueFactory(new PropertyValueFactory<Book, String>("dewey"));
+        bookList.getItems().addAll(MKLibrary.getLibrary());
+    }
+
+    public void openBookPage(MouseEvent e){
+        Book book = (Book) bookList.getSelectionModel().getSelectedItem();
+        if (book.getDewey() != selectedDewey){
+            System.out.println(book);
+        }
+        selectedDewey = book.getDewey();
+
+    }
 }
